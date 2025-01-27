ae15b59ee92c614b495848aacf81782a0cb4bfc2
mdasif8393 <md.asif.8393@gmail.com>
2025-01-27 11:32:12 +0600

    add error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
 import { BookRoutes } from "./app/modules/book/book.route";
 import { OrderRoutes } from "./app/modules/order/order.route";
 
@@ -16,5 +18,10 @@ app.use("/api/orders", OrderRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Book Shop is running");
 });
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
