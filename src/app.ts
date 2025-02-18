ef1d492c079cb04b4d704e93e7c8cc463290d75f
mdasif8393 <md.asif.8393@gmail.com>
2025-02-18 10:52:52 +0600

    complete

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://l2-a4-book-shop-frontend.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
